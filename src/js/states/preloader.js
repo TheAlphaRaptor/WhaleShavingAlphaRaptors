0b54b4a72c06e3c3bad6c9014d75d24c1a768619
Blaine Instone <blaine@instones.org>
2015-12-19 14:09:03 -0500

    first commit

--- src/js/states/preloader.js
+++ src/js/states/preloader.js
@@ -8,12 +8,13 @@ module.exports = Preloader;
 Preloader.prototype = {
 
   preload: function () {
-    this.asset = this.add.sprite(320, 240, 'preloader');
-    this.asset.anchor.setTo(0.5, 0.5);
+      this.asset = this.add.sprite(320, 240, 'preloader');
+      this.asset.anchor.setTo(0.5, 0.5);
 
-    this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
-    this.load.setPreloadSprite(this.asset);
-    this.load.image('testsprite', 'assets/test.png');
+      this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
+      this.load.setPreloadSprite(this.asset);
+      this.load.image('testsprite', 'assets/test.png');
+      this.load.image('anythingyouwant','assets/picture.jpeg')
   },
 
   create: function () {
