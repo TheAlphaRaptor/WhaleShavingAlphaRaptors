6694f00e3f9a95e3376faecd92bb9a2fd568768d
Blaine Instone <blaine@instones.org>
2016-01-23 13:08:52 -0500

    first commit

--- src/js/states/menu.js
+++ src/js/states/menu.js
@@ -14,8 +14,8 @@ Menu.prototype = {
 
     var style = { font: "65px Arial", fill: "#ffffff", align: "center" };
     background = this.game.add.tileSprite(0, 0, 800, 600, 'background');
-    button = this.game.add.button(200,50,"shave",  function() {
-
+    button = this.game.add.button(200,100,"razor",  function() {
+    //  sprite.scale.setTo(50, 12.5);
     }, this, 0,1,2 )
     this.input.onDown.add(this.onDown, this);
   },
