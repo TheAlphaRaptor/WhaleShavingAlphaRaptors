271807c0913ccdbdda58123afa8d4ea75a357fce
Blaine Instone <blaine@instones.org>
2016-04-09 12:56:39 -0400

    bu

--- src/js/states/game.js
+++ src/js/states/game.js
@@ -11,6 +11,8 @@ const textures = ["testsprite", "bananurai", "pupper"];
 game.prototype = {
   texture: 0,
   create: function () {
+    this.game.world.setBounds(0, 0, 800, 800);
+
     var x = (this.game.width / 2) - 100;
     var y = (this.game.height / 2) - 50;
     this.add.sprite(0,0,"Dan");
@@ -34,7 +36,8 @@ game.prototype = {
     this.game.physics.p2.restitution = 0.8;
     image = this.add.sprite(100, 100, 'Enemy');
     //this.physics.arcade.gravity.y=20;
-    player = this.add.sprite(1, 1, 'testsprite');
+    player = this.add.sprite(this.world.centerX, this.world.centerY, 'testsprite');
+    this.physics.enable(player, Phaser.Physics.P2JS);
     //player.collideWorldBounds = true;
     player.scale.setTo(.5,.5);
     player.inputEnabled = true;
@@ -49,7 +52,7 @@ game.prototype = {
     image.body.velocity.setTo(200,200);
     image.body.collideWorldBounds = true;
     player.body.collideWorldBounds = true;
-    this.game.world.setBounds(0, 0, 800, 800);
+    this.game.world.setBounds(0, 0, this.world.width, this.world.height);
    // this.physics.p2.restitution = 0.8;
    // image.smoothed=false;
    // player.smoothed=false;
@@ -57,9 +60,9 @@ game.prototype = {
     //this.world.setBounds(0,0,500,500);
 
     this.game.camera.follow(this.player);
-    var playerCollisionGroup = this.physics.p2.createCollisionGroup();
-    var narbrawlCollisionGroup = this.physics.p2.createCollisionGroup();
-    this.physics.p2.updateBoundsCollisionGroup();
+ //   var playerCollisionGroup = this.physics.p2.createCollisionGroup();
+  //  var narbrawlCollisionGroup = this.physics.p2.createCollisionGroup();
+  //  this.physics.p2.updateBoundsCollisionGroup();
 
   },
 
